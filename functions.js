90c7d8e99e23ce68c737f3613c9e3741ed928f5d
Jovic_Penhaz <victosousa92@gmail.com>
2025-05-05 21:01:47 -0300

    um: importar e exportar, package json e iniciais

---
+++ functions.js
@@ -0,0 +1,17 @@
+
+
+export function calculaMedia(n1, n2) {
+
+    (n1 + n2) = soma
+    mediaGeral = soma / 2
+
+    return (`a media é ${mediaGeral}`)
+}
+
+export function mostrarNome(nome){
+    console.log(`o nome é: ${nome}`)
+}
+
+// module.exports = {
+//     calculaMedia
+// }
---
+++ main.js
@@ -0,0 +1,8 @@
+import * as funcoes from "./functions.js"
+
+import {calculaMedia, mostrarNome} from "./functions.js"
+
+const funcoes = require("./functions")
+
+funcoes.calculaMedia(2, 2)
+
